54586405e8e49057d031b31cd4088a50000ea06e
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2023-08-27 13:51:39 +1000

    Loaded the data

--- Lab 6/Lab 6.R
+++ Lab 6/Lab 6.R
@@ -1,5 +1,17 @@
 library("shiny")
 
+auBirth <- read.csv("Births_summary_with_id.csv", stringsAsFactors = FALSE)
+
+# Convert data to long format for multiple data series (2017-2020)
+auBirth_long <- pivot_longer(auBirth, cols = starts_with("X"), names_to = "Year", values_to = "Births")
+
+# Remove X before years
+auBirth_long$Year <- gsub("X", "", auBirth_long$Year)
+
+# Sort the data by Births in descending order
+auBirth_long <- auBirth_long[order(auBirth_long$Region),]
+
+
 ##################
 # USER INTERFACE #
 ##################
@@ -11,7 +23,7 @@ births_tab <- tabPanel(
 )
 
 ui <- navbarPage(
-  title='Population growth in Australia',
+  title='Population Growth in Australia',
   births_tab
 )
 
