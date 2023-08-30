2ec5b3242e313a1580414ef017d6301531c67a3c
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2023-08-30 13:23:47 +1000

    Completed ex 6

--- Lab 6/Lab 6.R
+++ Lab 6/Lab 6.R
@@ -32,9 +32,16 @@ births_tab <- tabPanel(
   )
 )
 
+trends_tab <- tabPanel(
+  title='Trends',
+  h2('Births in Australia over the Years'),
+  plotOutput('plot_trends')
+)
+
 ui <- navbarPage(
   title='Population Growth in Australia',
-  births_tab
+  births_tab,
+  trends_tab
 )
 
 ################
@@ -61,6 +68,33 @@ server <- function(input, output, session) {
     scale_x_discrete(labels = function(x) str_wrap(x, width = 10))
     
     girafe(ggobj=p, height_svg=4)
+  })
+  
+  output$plot_trends <- renderPlot({
+    # Create a custom color palette with 8 colors
+    my_colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",
+                            "#9467bd", "#8c564b", "#e377c2", "#7f7f7f")
+                            
+    # Plot a line chart with additional customizations
+    ggplot(auBirth_long_new, aes(x = Year, y = Births, group = id, color = Region)) +
+      geom_line(linewidth = 1.5) +
+      scale_color_manual(values = my_colors) +
+      labs(title = "Birth Trends by Region in Australia (2017-2020)",
+           x = "Year",
+           y = "Number of Births") +
+      scale_x_discrete(labels = function(x) as.numeric(gsub("X", "", x))) +  # Remove "X" prefix
+      theme_minimal() +
+      theme(plot.title = element_text(size = 16, face = "bold"),
+            axis.text = element_text(size = 12),
+            axis.title = element_text(size = 14),
+            legend.background = element_rect(fill = "transparent"),  # Remove legend background
+            legend.box.background = element_rect(color = "black"),  # Add border to legend box
+            legend.title = element_text(size = 14),
+            legend.text = element_text(size = 12),
+            panel.background = element_rect(fill = "#f0f0f0"),  # Set panel background color
+            panel.grid.major = element_line(color = "white"),  # Customize grid lines
+            panel.grid.minor = element_line(color = "white")) +
+      guides(color = guide_legend(override.aes = list(size = 2)))  # Adjust legend symbol size
   })
 }
 
